d9250cd026a47af43a6d9c53a9ef2551aa820a65
Junior Paula <luke.junnior@icloud.com>
2022-09-03 10:10:03 -0300

    feat: configurate server

---
+++ src/server.js
@@ -0,0 +1,24 @@
+require('dotenv/config');
+const express = require('express');
+const cors = require('cors');
+const http = require('http');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+// eslint-disable-next-line no-unused-vars
+const io = socketio(server, {
+  cors: {
+    origin: process.env.CLIENT_URL,
+    methods: ['POST', 'GET'],
+  },
+});
+
+// const rooms = ['geral', 'RH', 'financeiro', 'desenvolvimento'];
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT;
+server.listen(PORT, () => console.info(`Server is running on port ${PORT}`));
