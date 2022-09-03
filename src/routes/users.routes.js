74607d3ac9e42a798dffef2063438c90af6e1ff3
Junior Paula <luke.junnior@icloud.com>
2022-09-03 15:57:45 -0300

    feat: create user routes

---
+++ src/routes/users.routes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const User = require('../database/mongodb/models/users');
+
+router.post('/', async (req, res) => {
+  try {
+    const { name, email, password, picture } = req.body;
+    const user = await User.create({ name, email, password, picture });
+    res.status(201).json(user);
+  } catch (error) {
+    let msg;
+    if (error.code == 11000) {
+      msg = 'Usuário ja existe';
+    } else {
+      msg = error.message;
+    }
+    res.status(400).json(msg);
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = User.findByCredentials(email, password);
+    user.status = 'online';
+    await user.save();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+});
